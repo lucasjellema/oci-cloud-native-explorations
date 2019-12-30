10e69183e3c98cb70f74fabe2d2f1e617729db72
Lucas Jellema <lucasjellema@gmail.com>
2019-12-30 07:02:44 +0000

    Hardened authorizer function a little; test for even/odd in security token/header

--- client-secret-authorizer/authorizer.js
+++ client-secret-authorizer/authorizer.js
@@ -1,16 +1,34 @@
 function authorizeClientSecret(token) {
-    const expiryInterval = 3600000 // one hour expressed in miliseconds
-    const expiry = new Date( Date.now() + expiryInterval );
-    const json = {
-        "active": true,
-        "principal": "Henk",
-        "scope": ["list:hello", "read:hello", "create:hello", "update:hello", "delete:hello", "someScope"],
-        "clientId": "clientIdFromHeader",
-        "expiresAt": expiry.toISOString(),
-        "context": {
-          "key": "value", "email":"me@mail.com" ,"input": token
+    //const expiryInterval = 3600000 // one hour expressed in miliseconds
+    const expiryInterval = 60000 // one minute expressed in miliseconds
+    const expiry = new Date(Date.now() + expiryInterval);
+    let json
+    // if the last character in the token is a number and the number is even, then authorization succeeds
+    const lastCharacter = token.slice(-1)
+    if (!isNaN(lastCharacter) && ((parseInt(lastCharacter) % 2) == 0)) {
+        json = {
+            "active": true,
+            "principal": "Henk",
+            "scope": ["list:hello", "read:hello", "create:hello", "update:hello", "delete:hello", "someScope"],
+            "clientId": "clientIdFromHeader",
+            "expiresAt": expiry.toISOString(),
+            "context": {
+                "key": "value", "email": "me@mail.com", "input": token
+            }
         }
-      }
+    }
+    else
+        // simulate authorization error
+        json =
+            {
+                "active": false,
+                "expiresAt": expiry.toISOString(),
+                "context": {
+                    "email": "james.doe@example.com"
+                    , "input": token
+                },
+                "wwwAuthenticate": "Bearer realm=\"lucas.jellema.com\""
+            }
     return json
 }
 
@@ -20,10 +38,11 @@ module.exports = {
 }
 
 // invoke on the command line with :
-// node authorizer '{"type":"TOKEN","token":"secret"}'
+// node authorizer '{"type":"TOKEN","token":"secret2"}'
 run = async function () {
     if (process.argv && process.argv[2]) {
-        let response =  authorizeClientSecret(process.argv[2])
+        const input = JSON.parse(process.argv[2])
+        let response = authorizeClientSecret(input.type=='TOKEN'?input.token:input.header)
         console.log("response: " + JSON.stringify(response))
     }
 }
--- client-secret-authorizer/func.js
+++ client-secret-authorizer/func.js
@@ -1,31 +1,30 @@
-const fdk=require('@fnproject/fdk');
-const authorizer = require( './authorizer.js' );
+const fdk = require('@fnproject/fdk');
+const authorizer = require('./authorizer.js');
 
-fdk.handle(function(input, ctx){
-   let x = authorizer.authorizeClientSecret(input)
-   ctx.setResponseHeader('Fn-Http-Status', 200)
-   ctx.setResponseHeader("Greeting", "Goedendag")
-   ctx.setResponseHeader("secret-header", input.header)
-   ctx.setResponseHeader("secret-token", input.token)
-   ctx.setResponseHeader("secret-type", input.type)
-   if (Math.random() < 0.4) {
-     // simulate authorization error
-     x=
-     {
+fdk.handle(function (input, ctx) {
+  if (!input || !input.type || !(input.type == 'TOKEN' ? input.token : input.header)) {
+    // no token is passed in
+    const x =
+    {
       "active": false,
       "expiresAt": "2019-05-30T10:15:30+01:00",
       "context": {
         "email": "james.doe@example.com"
-        , "input" : input
-        },
-      "wwwAuthenticate": "Bearer realm=\"lucas.jellema.com\""
+        , "input": input
+      },
+      "wwwAuthenticate": "Bearer realm=\"provide.token.next.time.round\" "
     }
     // this status causes the authorization to fail and the API Gateway to reject the request with a 401 to the original caller
-    ctx.setResponseHeader('Fn-Http-Status', 500)
-    
-   }
+    //ctx.setResponseHeader('Fn-Http-Status', 500)
+    return x
+  }
+  const x = authorizer.authorizeClientSecret(input.type == 'TOKEN' ? input.token : input.header)
+  ctx.setResponseHeader("Greeting", "Goedendag")
+  ctx.setResponseHeader("secret-header", input.header)
+  ctx.setResponseHeader("secret-token", input.token)
+  ctx.setResponseHeader("secret-type", input.type)
   return x
 })
 
 // invoke with :
-// echo -n '{"type":"TOKEN","token":"secret"}' | fn invoke lab-app client-secret-authorizer
+// echo -n '{"type":"TOKEN","token":"secret2"}' | fn invoke lab-app client-secret-authorizer
